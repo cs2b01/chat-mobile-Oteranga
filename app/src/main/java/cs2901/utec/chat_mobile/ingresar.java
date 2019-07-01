c90ea0b5b71aee50a7e2b00cac4ec2a4b0bd5d53
Oteranga <40015080+Oteranga@users.noreply.github.com>
2019-06-30 23:56:08 -0500

    Create ingresar.java

---
+++ app/src/main/java/cs2901/utec/chat_mobile/ingresar.java
@@ -0,0 +1,10 @@
+package cs2901.utec.chat_mobile;
+
+import android.widget.Toast;
+
+public class ingresar {
+    public void showMessage(String message) {
+        Toast.makeText(,Toast.LENGTH_LONG).show();
+    }
+    
+    
